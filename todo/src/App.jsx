8fbac21c3ed6587690a9b34c6bbcfad88d70c55f
jitendra singh <jitendrasngh58@gmail.com>
2024-11-04 18:46:44 +0530

    files

---
+++ todo/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Todos from './app/components/Todos'
+import './App.css'
+import AddTodo from './app/components/AddTodo'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Learn about redux ToolKit</h1>
+     <AddTodo/>
+     <Todos/>
+
+
+    </>
+  )
+}
+
+export default App
